93fe3dae0e7c9848640f1570cd90c2ac5ddea03b
KalaPhantom <laphantom40@gmail.com>
2024-05-16 15:59:47 +0800

    Add project files.

---
+++ Log and Sign Up/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log_and_Sign_Up
+{
+    class DataContainerClass
+    {
+        private SortedList userDataContainer = new SortedList();
+
+        private void DefaultData()
+        {
+            userDataContainer.Add("allen300","12345678");
+            userDataContainer.Add("Kala_Phantom","coding@last");
+            userDataContainer.Add("ExoticButters","FreedieFazbear");
+        }
+
+        public void InitializeDefaultData()
+        {
+            DefaultData();  
+        }
+
+        public void ValidateData()
+        {
+            string userPassword = Program.UserPassword;
+            string userName = Program.UserName;
+
+            if (userDataContainer.ContainsKey(userName) && userDataContainer.ContainsValue(userPassword))
+            {
+                
+            }
+        }
+
+     
+    }
+}
